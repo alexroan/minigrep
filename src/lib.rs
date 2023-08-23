7602d25998b22b9a2867e8c3ba1b1d7e163d87a3
Alex Roan <alex.roan@hotmail.com>
2023-08-23 16:54:13 +0100

    Search function

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,6 @@ pub struct Config {
     pub file_path: String,
 }
 
-// Box<dyn Error> means the function will return a type that implements the Error trait, dyn means dynamic
-// We don't actually know what the Error is (inside read_to_string), but we know it implements the Error trait
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-
 // Impl is like a library in Solidity.
 impl Config {
     // This enables Config::build to be called.
@@ -32,5 +21,47 @@ impl Config {
 
         // Return a Config struct
         Ok(Config { query, file_path })
+    }
+}
+
+// Box<dyn Error> means the function will return a type that implements the Error trait, dyn means dynamic
+// We don't actually know what the Error is (inside read_to_string), but we know it implements the Error trait
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+// Search through contents for query
+// lifetime of the return value is the same as the lifetime of the contents argument because we return a slice of contents
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,14 @@ fn main() {
 
     // Must unwrap the Result type returned by Config::build
     // This block enables a graceful exit if the build fails
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+    let config = Config::build(&args).unwrap_or_else(|_err| {
         // Exit with a non-zero exit code
         std::process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     // If run returns an error, print it and exit
     // run(config) by itself returns a warning because it's not being used
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+    if let Err(_e) = minigrep::run(config) {
         std::process::exit(1);
     }
 }
