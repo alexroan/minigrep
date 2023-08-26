beef2621aacf8b7b2612f2f7a52ae30af7f36d18
Alex Roan <alex.roan@hotmail.com>
2023-08-26 22:56:45 +0100

    Using iterators instead of loops

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,37 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 // Impl is like a library in Solidity.
 impl Config {
     // This enables Config::build to be called.
     // Return type: Result: Ok or Err, Config or &str
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            // String error
-            return Err("Not enough arguments");
-        }
-        // must clone these because args is a borrowed reference
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        // Return a Config struct
-        Ok(Config { query, file_path })
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -42,7 +53,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 // Search through contents for query
 // lifetime of the return value is the same as the lifetime of the contents argument because we return a slice of contents
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
     // for line in contents.lines() {
     //     if line.contains(query) {
@@ -51,9 +62,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     // }
 
     // iterator version
-    contents.lines().for_each(|line| if line.contains(query) { results.push(line); });
-
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ use std::env;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
     // Must unwrap the Result type returned by Config::build
     // This block enables a graceful exit if the build fails
-    let config = Config::build(&args).unwrap_or_else(|_err| {
+    let config = Config::build(env::args()).unwrap_or_else(|_err| {
         // Exit with a non-zero exit code
         std::process::exit(1);
     });
